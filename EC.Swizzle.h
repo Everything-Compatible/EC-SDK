81a7f357414c7488d358855fc09f58686788c91c
Ironhammer Std <2482911962@qq.com>
2025-10-11 10:45:09 +0800

    Compatible With Old yrpp

--- EC.Swizzle.h
+++ EC.Swizzle.h
@@ -19,13 +19,13 @@ public:
 	// 把需要改变地址的指针放在这里 , 在调用函数后 *指针 的值会变成 nullptr
 	HRESULT RegisterForChange(void** 指针)
 	{
-		return SwizzleManagerClass::Instance().Swizzle(指针);
+		return SwizzleManagerClass::Instance->Swizzle(指针);
 	}
 
 	// 原版游戏中的所有对象都会保存它的 this 指针的值到输出流中 , 然后通过调用这个函数来记录保存的 this 指针的值和新的指针的值
 	HRESULT RegisterChange(void* 旧的指针, void* 新的指针)
 	{
-		return SwizzleManagerClass::Instance().Here_I_Am((long)旧的指针, 新的指针);
+		return SwizzleManagerClass::Instance->Here_I_Am((long)旧的指针, 新的指针);
 	}
 
 	template<typename T>
